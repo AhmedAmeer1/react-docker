d447cde7e635727ae008da4cb30c6ad114e2454a
@ahmed.creativehub <ahmed.creativehub@gmail.com>
2022-05-17 18:54:15 +0530

    frontend updated

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello world ahmed
+         Hello User
         </p>
         <a
           className="App-link"
